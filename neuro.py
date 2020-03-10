193fbb3f013a5fa70b1f06ec1801cb30575c0971
s.shlyikov <mpeegy@ya.ru>
2020-03-10 09:54:56 +0300

    комент нейросети

--- neuro.py
+++ neuro.py
@@ -8,8 +8,10 @@ random.seed(1)
 synaptic_weights = 2 * random.random((3, 1)) - 1
 
 for iteration in range(10000):
+    # sinaps
     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
+#sinaps
 print (1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights)))))
 
 input()
