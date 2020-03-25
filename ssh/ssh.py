f1500bab2dd6807a2f54827faaceb544d01c554c
s.shlyikov <mpeegy@ya.ru>
2020-03-25 20:55:01 +0300

    ssh connection

---
+++ ssh/ssh.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import paramiko
+hostname = '192.168.1.15'
+port = 22
+username = 'jmjones'
+password = 'xxxYYYxxx'
+if __name__ == "__main__":
+    paramiko.util.log_to_file('paramiko.log')
+    s = paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.connect(hostname, port, username, password)
+    stdin, stdout, stderr = s.exec_command('ifconfig')
+    print (stdout.read())
+    s.close()
